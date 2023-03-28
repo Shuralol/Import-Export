d2e9140a35e10d35c626d22e6f2bdb700bb44f1b
Shuralol <akinfeev09@bk.ru>
2023-03-28 14:25:49 +0700

    задание №2 не работает

---
+++ game.js
@@ -0,0 +1,21 @@
+import Character from "./domain";
+
+export class GameSavingData {}
+
+export function readGameSaving() {}
+
+export function writeGameSaving() {}
+
+export default class Game {}
+
+class Game {
+  start() {
+    console.log("game started");
+  }
+}
+
+class GameSavingData {}
+
+function readGameSaving() {}
+
+function writeGameSaving() {}
